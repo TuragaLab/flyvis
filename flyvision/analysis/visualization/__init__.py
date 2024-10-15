1707bb092b804b39dd7438557ce83c6c8a2b4bdd
Janne Lappalainen <lappalainenjk@gmail.com>
2024-10-15 18:04:31 -0400

    rm propr fonts

--- flyvision/analysis/visualization/__init__.py
+++ flyvision/analysis/visualization/__init__.py
@@ -8,8 +8,9 @@ import matplotlib.pyplot as plt
 
 from matplotlib.font_manager import fontManager
 
-for font in (Path(__file__).parent.parent.parent / ".fonts").glob("*.ttf"):
-    fontManager.addfont(font)
+if (fonts := Path(__file__).parent.parent.parent / ".fonts").exists():
+    for font in fonts.glob("*.ttf"):
+        fontManager.addfont(font)
 matplotlib.rc("figure", dpi=300)
 matplotlib.rc("font", **{"family": "sans-serif", "sans-serif": "Arial", "size": 6})
 
